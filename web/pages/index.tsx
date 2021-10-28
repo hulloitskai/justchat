a4ef62d4f41252f90c1c7a3efe86950ff8ce0eb5
Kai <hulloitskai@gmail.com>
2021-10-28 10:51:03 -0400

    🐛 (web) Fix input jitter on mobile

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -2,7 +2,7 @@ import React, { useMemo, useRef, useState } from "react";
 import type { NextPage } from "next";
 import detect from "browser-detect";
 
-import { Container, VStack } from "@chakra-ui/react";
+import { Container, Box, VStack } from "@chakra-ui/react";
 import { Text } from "@chakra-ui/react";
 import { Input } from "@chakra-ui/react";
 import { Alert, AlertIcon, AlertDescription } from "@chakra-ui/react";
@@ -33,46 +33,52 @@ const HomePage: NextPage = () => {
         </Text>
         <HandlePicker onPick={setHandle} />
         <Chat handle={handle} flex={1} />
-        <Alert status={handle ? "info" : "warning"} rounded="md" px={3} py={2}>
-          <AlertIcon />
-          <AlertDescription fontSize="sm">
-            {handle ? (
-              <>
-                Start typing!
-                {isMobile && (
-                  <>
-                    {" "}
-                    <chakra.span
-                      fontWeight="medium"
-                      textDecor="underline"
-                      onClick={() => {
-                        const inputEl = inputRef.current;
-                        if (inputEl) {
-                          inputEl.value = "";
-                          inputEl.focus();
-                        }
-                      }}
-                    >
-                      Click here to open keyboard.
-                    </chakra.span>
-                  </>
-                )}
-              </>
-            ) : (
-              "You are anonymous; this conversation is read-only."
-            )}
-          </AlertDescription>
-        </Alert>
+        <Box pos="relative">
+          <Alert
+            status={handle ? "info" : "warning"}
+            rounded="md"
+            px={3}
+            py={2}
+          >
+            <AlertIcon />
+            <AlertDescription fontSize="sm">
+              {handle ? (
+                <>
+                  Start typing!
+                  {isMobile && (
+                    <>
+                      {" "}
+                      <chakra.span
+                        fontWeight="medium"
+                        textDecor="underline"
+                        onClick={() => {
+                          const inputEl = inputRef.current;
+                          if (inputEl) {
+                            inputEl.value = "";
+                            inputEl.focus();
+                          }
+                        }}
+                      >
+                        Click here to open keyboard.
+                      </chakra.span>
+                    </>
+                  )}
+                </>
+              ) : (
+                "You are anonymous; this conversation is read-only."
+              )}
+            </AlertDescription>
+          </Alert>
+          <Input
+            ref={inputRef}
+            pos="absolute"
+            inset={0}
+            zIndex="-1"
+            border="none"
+            pointerEvents="none"
+          />
+        </Box>
       </VStack>
-      <Input
-        ref={inputRef}
-        pos="absolute"
-        left={0}
-        right={0}
-        top={-24}
-        bottom="unset"
-        pointerEvents="none"
-      />
     </Container>
   );
 };
